eed40f746e5c7b26e7d60be8871a1308d71813ee
Ashley Barrett <ashley.barrett@judopayments.com>
2016-11-16 13:19:06 +0000

    initial

---
+++ Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let SayHello me = printfn "Hello %s" me
+
+let Square num = num * num
+
+[<EntryPoint>]
+let main argv = 
+    SayHello "Ash"
+    let res = Square 6
+    res = 1
+    printfn "%i" res
+    0 // return an integer exit code
