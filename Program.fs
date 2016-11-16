983dc824cab7256712c3c17f1f3f0e976b429c18
Ashley Barrett <ashleybarrett@Ashleys-MacBook-Pro.local>
2016-11-16 22:26:48 +0000

    latest

--- Program.fs
+++ Program.fs
@@ -2,14 +2,30 @@
 
 open System
 
-let SayHello me = printfn "Hello %s" me
+let Sqrs arr =
+    let Sqr num = num * num
+    [ for num in arr do yield Sqr num ]
 
-let Square num = num * num
+let IsEven num = num % 2 = 0
+
+type GeoCoord = { lat: int; long: int; named:string option }
+
+let PrintGeoCord geoCoord = 
+    let name = 
+        match geoCoord.named with
+        | Some n -> n
+        | None -> "Not sure"
+    printfn "%i %i %s" geoCoord.lat geoCoord.long name     
 
 [<EntryPoint>]
 let main argv = 
-    SayHello "Ash"
-    let res = Square 6
-    res = 1
-    printfn "%i" res
+
+    Sqrs [|1..100|]    
+    |> List.filter IsEven
+    |> List.iter(fun x -> printfn "%i" x)
+
+    { lat = 1; long = 2; named = Some "London" } |> PrintGeoCord
+
+    { lat = 1; long = 2; named = None } |> PrintGeoCord
+
     0 // return an integer exit code
