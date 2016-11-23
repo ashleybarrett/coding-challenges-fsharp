80ff8798cf098c4bdca522714149fc6105615263
Ashley Barrett <ashley.barrett@judopayments.com>
2016-11-23 09:33:17 +0000

    push

--- HackerrankChallanges/Challange8.fs
+++ HackerrankChallanges/Challange8.fs
@@ -13,18 +13,13 @@ let read_and_parse()=
 let main =
     let evalulate times input =
         [0..(times-1)]
-        |> List.fold (fun x y -> x + (Math.Pow(input, float y)/ float y)) 0.0
-        |> printfn "%f" 
+        |> List.fold (fun x y -> x + y) 0
+        |> printfn "%d" 
 
-    (*let evalulate times input =
-        [0..times]
-        |> List.fold(fun acc x -> sprintf "%i %d" acc x) 0 
-        *)
     let input = read_and_parse()
-    let times = input.[0]
+    let times = int input.[0]
 
     input.[1..]
-    |> List.map(fun x -> abs x)
-    |> List.iter(fun x -> (evalulate (int times) x)) 
+    |> List.iter(fun x -> (evalulate times x)) 
 
 main
