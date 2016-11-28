c0e1c42918a682bd801899c59fb0581b9326b1cf
Ashley Barrett <ashleybarrett@Ashleys-MacBook-Pro.local>
2016-11-28 22:27:16 +0000

    c

--- ProjectEulerChallanges/Challange2.fs
+++ ProjectEulerChallanges/Challange2.fs
@@ -4,9 +4,18 @@ open System
 let main argv = 
     
     //https://projecteuler.net/problem=2
-    
-    [1..10]
-    |> List.fold (fun x y -> x + y) 0
+
+    let doesNotExceed = 4000000
+
+    Seq.unfold(
+            fun (a,b) -> 
+                let next = a + b
+                match next <= doesNotExceed with 
+                | true -> Some(next, (b, next))
+                | _ -> None
+    ) (0,1)
+    |> Seq.filter(fun x -> x % 2 = 0)
+    |> Seq.sum
     |> printfn "%i"
 
     0 
--- ProjectEulerChallanges/Challange3.fs
+++ ProjectEulerChallanges/Challange3.fs
@@ -4,13 +4,18 @@ open System
 let main argv = 
     
     //https://projecteuler.net/problem=3
-    
+    let limit = 10
+
     let isPrime n =
         match n with
-        | _ when n >= 3 -> true
-        | _ -> false
+        | _ when n = 1 -> false
+        | _ when n = 2 -> true
+        | _ when n % 2 = 0 -> false
+        | _ -> true  
 
-    [1..10]
-    |> List.filter(fun n -> isPrime n)
-    |> List.iter(fun n -> printfn "%i" n)
+    [1..limit]
+    |> List.iter(fun n -> printfn "%i %b" n (isPrime n))
+    //|> List.filter(fun n -> isPrime n)
+    //|> List.iter(fun n -> printfn "%i" n)
+    
     0 
