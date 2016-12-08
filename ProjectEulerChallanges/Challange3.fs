540f1f825038587825da47c02fbec8a4a5b65da5
Ashley Barrett <ashley.barrett@judopayments.com>
2016-12-08 19:03:36 +0000

    l

--- ProjectEulerChallanges/Challange3.fs
+++ ProjectEulerChallanges/Challange3.fs
@@ -4,22 +4,29 @@ open System
 let main argv = 
     
     //https://projecteuler.net/problem=3
-    let num: int64 = 600851475143L
+    //let num: int64 = 600851475143L
+    let num = 13195
+
+
+    (*
+    let rec largestPrimeFactor numbers = 
+        match numbers with
+        | head::tail when isPrime head tail && num % head = 0L -> true
+        | _::tail -> largestPrimeFactor tail
+        | [] -> false
+        | _ -> false *)
 
     let rec isPrime n list = 
         match list with
-        | _ when n = 1L -> false
-        | _ when n = 2L -> true
-        | _ when n % 2L = 0L -> false
-        | head::_ when n % head = 0L -> false
+        | head::_ when n % head = 0 -> false
         | head::tail -> isPrime n tail
         | [] -> true
         | _ -> false
 
     let factors = 
-        [1L..num]
-        |> List.filter(fun n -> (isPrime n [2L..(n-1L)]))
-        |> List.filter(fun n -> num % n = 0L)
+        [3..2..num]
+        |> List.filter(fun n -> isPrime n [2..(n-1)])
+        |> List.filter(fun n -> num % n = 0)
         |> List.rev
 
     printfn "%i" factors.[0]
