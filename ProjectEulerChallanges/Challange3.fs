fcd10442fc7e630ae6209eea3ec2da753d6e56f2
Ashley Barrett <ashleybarrett@Ashleys-MacBook-Pro.local>
2016-12-11 20:15:18 +0000

    latest .

--- ProjectEulerChallanges/Challange3.fs
+++ ProjectEulerChallanges/Challange3.fs
@@ -4,19 +4,25 @@ open System
 let main argv = 
     
     //https://projecteuler.net/problem=3
-    //let num: int64 = 600851475143L
-    let num = 13195L
+    let num: int64 = 600851475143L
+    //let num = 13195L
 
-    let rec isPrime n list = 
-        match list with
-        | head::_ when n % head = 0L -> false
-        | head::tail -> isPrime n tail
-        | [] -> true
-        | _ -> false
+    let hasFactors n = 
+        let upperBound = int64(Math.Sqrt(double(n)))
+        let factors =
+            [3L..2L..upperBound]
+            |> List.filter(fun i -> n % i = 0L)
+        factors.Length = 0
+
+    let isPrime n = 
+        match n with
+        | 1L -> false
+        | 2L -> true
+        | _ -> hasFactors n
 
     let factors = 
         [3L..2L..num]
-        |> List.filter(fun n -> isPrime n [2L..(n-1L)])
+        |> List.filter(fun n -> isPrime n)
         |> List.filter(fun n -> num % n = 0L)
         |> List.rev
 
