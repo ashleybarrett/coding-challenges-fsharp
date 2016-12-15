04e1ac37ad4ec3c3af455089e5cae6833c7b1229
Ashley Barrett <ashley.barrett@judopayments.com>
2016-12-15 09:38:30 +0000

    challange 9

--- ProjectEulerChallanges/Challange9.fs
+++ ProjectEulerChallanges/Challange9.fs
@@ -5,4 +5,14 @@ let main argv =
     
     //https://projecteuler.net/problem=9
 
+    let number = 25
+
+    let squared n = n * n
+
+    let squares = 
+        [1..number]
+        |> List.iter(fun n -> squared n) 
+
+    
+
     0 
