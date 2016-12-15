75b958e80c149a9bb68f5653651527752c15636c
Ashley Barrett <ashley.barrett@judopayments.com>
2016-12-15 12:02:43 +0000

    update

--- ProjectEulerChallanges/Challange10.fs
+++ ProjectEulerChallanges/Challange10.fs
@@ -7,19 +7,13 @@ let main argv =
 
     let number = 10L
 
-    let hasFactors n = 
+    let isPrime n = 
         let upperBound = int64(Math.Sqrt(double(n)))
         let factors =
             [3L..2L..upperBound]
             |> List.filter(fun i -> n % i = 0L)
         int64(factors.Length) = 0L
-
-    let isPrime n = 
-        match n with
-        | 1L -> false
-        | 2L -> true
-        | _ -> hasFactors n
-
+        
     let primesSum = 
         [3L..2L..2000000L]
         |> List.filter(fun n -> isPrime n)
