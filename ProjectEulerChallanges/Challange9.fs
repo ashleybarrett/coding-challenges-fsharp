43e5eb416e83b2834ddb54dfd3d75de3691f688f
Ashley Barrett <ashleybarrett@Ashleys-MacBook-Pro.local>
2016-12-28 17:15:56 +0000

    challange 9

--- ProjectEulerChallanges/Challange9.fs
+++ ProjectEulerChallanges/Challange9.fs
@@ -5,12 +5,32 @@ let main argv =
     
     //https://projecteuler.net/problem=9
 
-    let squared n = n * n
+    let squared n = n * n 
 
-    let numbers = [0..1000]    
+    let number = 1000
 
-    let triplets = 
-        numbers
-    
+    let triple m n =
+        let mSquared = squared m
+        let nSquared = squared n
+        
+        let a = mSquared - nSquared 
+        let b = (m*n) * 2 
+        let c = mSquared + nSquared 
+
+        (a,b,c)
+
+    let isTriple (a,b,c) = (squared a) + (squared b) = (squared c) 
+
+    let isCandidate (a,b,c) = a+b+c = number
+
+
+    let a, b, c = 
+        [for m in [2..number] do
+                for n in [2..m] do
+                    yield triple m n] 
+        |> List.filter(fun (a,b,c) -> isTriple (a,b,c))
+        |> List.find(fun (a,b,c) -> isCandidate (a,b,c))
+
+    printfn "%d" <| a*b*c
 
     0 
