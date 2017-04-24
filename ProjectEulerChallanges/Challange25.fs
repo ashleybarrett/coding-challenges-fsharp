ae71ba8be658e16c943b535b87241d94b95e91d0
Ashley Barrett <ashleybarrett@Ashleys-MacBook-Pro.local>
2017-04-24 20:22:49 +0100

    l

--- ProjectEulerChallanges/Challange25.fs
+++ ProjectEulerChallanges/Challange25.fs
@@ -5,10 +5,14 @@ let main argv =
 
     //https://projecteuler.net/problem=25
     
-    List.unfold(fun (current,total) ->  
-        if current > 20 then None else Some(current,(current+1,total+current))) (1,0) 
-    |> List.rev
-    |> List.head
-    |> printf "%i"
+    List.unfold(fun (current, next) ->  
+        //printfn "%A %A" current next
+        let total = current + next
+        let lengthAsString = total.ToString()
+        if lengthAsString.Length <= 1000 then Some(current,(next, total)) else None
+    ) (1I,1I) 
+    |> List.length
+    //|> (-)1
+    |> printfn "%i"
 
     0  
