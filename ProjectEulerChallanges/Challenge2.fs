1e6685c510a16d2ccac56da4200dbfb118272dc7
Ashley Barrett <ashleybarrett@Ashleys-MacBook-Pro.local>
2017-05-02 10:06:46 +0100

    challenge 2

---
+++ ProjectEulerChallanges/Challenge2.fs
@@ -0,0 +1,16 @@
+open System
+
+[<EntryPoint>]
+let main argv = 
+
+    //https://projecteuler.net/problem=2
+
+    Seq.unfold(fun (l, r) ->
+        let next = l + r
+        if next <= 4000000 then Some(next, (r, next)) else None
+    ) (0,1)
+    |> Seq.filter(fun n -> n % 2 = 0)
+    |> Seq.sum
+    |> printfn "%i"
+
+    0 
