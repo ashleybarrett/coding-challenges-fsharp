1f7a16692d02fb2d9d6bc121d15eb359609bf323
Ashley Barrett <ashleybarrett@Ashleys-MacBook-Pro.local>
2017-05-04 16:04:18 +0100

    completed encryption challange

--- HackerrankChallanges/AlgorithmsTrack/Implementation/encryption.fs
+++ HackerrankChallanges/AlgorithmsTrack/Implementation/encryption.fs
@@ -5,8 +5,25 @@ let main argv =
 
     //https://www.hackerrank.com/challenges/encryption
 
-    //Unfinished
+    let input  = Console.ReadLine() |> string
+    let numberOfColumns = 
+        input.Length
+        |> float 
+        |> Math.Sqrt 
+        |> Math.Ceiling
+        |> int
 
-    
- 
+    input.ToCharArray()
+    |> Array.toSeq
+    |> Seq.map(string)
+    |> Seq.mapi (fun i v -> i % numberOfColumns, v)
+    |> Seq.groupBy fst
+    |> Seq.map(fun (i,v) -> 
+        v 
+        |> Seq.map(snd)
+        |> Seq.reduce(+)
+    )
+    |> Seq.reduce(fun ac el -> ac + " " + el)
+    |> printfn "%s"
+
     0
