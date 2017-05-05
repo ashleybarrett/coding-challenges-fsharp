20a425293883504d17dd58becfc944c86c7bcb42
Ashley Barrett <ashleybarrett@Ashleys-MacBook-Pro.local>
2017-05-05 21:34:27 +0100

    latest

--- HackerrankChallanges/AlgorithmsTrack/Implementation/the-grid-search.fs
+++ HackerrankChallanges/AlgorithmsTrack/Implementation/the-grid-search.fs
@@ -1,15 +1,43 @@
 open System
 
+type TestCase = { 
+    columnsRows: int * int;
+    grid: int[][];
+    patternColumnsRows: int * int;
+    patternGrid: int[][];
+}
+
 [<EntryPoint>]
 let main argv = 
 
     //https://www.hackerrank.com/challenges/the-grid-search
 
+    //Unfinished
+
     let numberOfTestCases = Console.ReadLine() |> int
 
-    let testCases = 
-        Seq.init numberOfTestCases (fun n ->
-            
+    let mapArray (s:string) = s.Split [|' '|] |> Array.map(int)
+
+    let generateTestCase = 
+        let columnsAndRows = Console.ReadLine() |> string |> mapArray
+        let grid = Array.init columnsAndRows.[1] (fun n ->
+           Console.ReadLine() |> string |> mapArray 
         )
+
+        let patternColumnsRows = Console.ReadLine() |> string |> mapArray
+        let patternGrid = Array.init patternColumnsRows.[1] (fun n ->
+           Console.ReadLine() |> string |> mapArray 
+        )
+
+        { 
+            columnsRows= (columnsAndRows.[0],columnsAndRows.[1]);
+            grid= grid;
+            patternColumnsRows= (patternColumnsRows.[0],patternColumnsRows.[1]);
+            patternGrid= patternGrid;
+        }
+        
+    let testCases = Array.init numberOfTestCases (fun n -> generateTestCase)
+
+    printfn "%A" testCases
 
     0
