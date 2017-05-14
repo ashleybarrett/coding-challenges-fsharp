360a5383dc1fb1922f37b4bcb24d033e1e644555
Ashley Barrett <ashleybarrett@Ashleys-MacBook-Pro.local>
2017-05-14 17:45:47 +0100

    challange progress

--- HackerrankChallanges/AlgorithmsTrack/Implementation/bigger-is-greater.fs
+++ HackerrankChallanges/AlgorithmsTrack/Implementation/bigger-is-greater.fs
@@ -7,4 +7,27 @@ let main argv =
 
     //Unfinished
 
+    let reduceToWord charList = charList |> List.reduce(+)
+
+    let rec wordPermutations charList originalWord =
+        let wordFromChars = reduceToWord charList
+        printfn "%s" wordFromChars
+
+        let nextCharList = List.append charList.Tail [charList.Head]
+        let nextWord = reduceToWord nextCharList
+
+        match nextWord with
+        | w when w <> originalWord -> wordPermutations nextCharList originalWord
+        | _ -> ()
+
+    
+    let word = "post"
+
+    let charsFromWord = 
+        word.ToCharArray()
+        |> Array.map(string)
+        |> Array.toList
+
+    wordPermutations charsFromWord word
+
     0
