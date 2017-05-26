81be97e4a515c6202401338dbdde8d4d329bc00b
Ashley Barrett <ashleybarrett@Ashleys-MacBook-Pro.local>
2017-05-26 13:08:11 +0100

    problem 10

---
+++ ProjectEulerChallanges/problem10.fs
@@ -0,0 +1,29 @@
+open System
+
+[<EntryPoint>]
+let main argv = 
+
+    //https://projecteuler.net/problem=10
+
+    let getSqrt n = n |> float |> sqrt |> int64
+
+    let isPrime n =
+        seq {
+            let sqrt = n |> float |> sqrt |> int64
+            for i in 2L .. sqrt do yield i
+        }
+        |> Seq.forall(fun x -> n % x <> 0L)
+
+    let upperValue = 2000000L
+
+    seq {
+        yield 2L
+        for i in 3L..2L..upperValue do
+            match isPrime i with
+            | true -> yield i
+            | false -> yield 0L
+    }
+    |> Seq.sum
+    |> printfn "%i" 
+
+    0
