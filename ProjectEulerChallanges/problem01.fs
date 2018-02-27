0f7c5a0eece83f65bd213cc5ca153c4ae5bc107d
Ashley Barrett <ashleybarrett.mail@googlemail.com>
2018-02-27 15:48:24 +0000

    solution one

--- ProjectEulerChallanges/problem01.fs
+++ ProjectEulerChallanges/problem01.fs
@@ -4,4 +4,8 @@ let solution =
 
     //https://projecteuler.net/problem=1
 
-    "Hello"
+    let isMultipleOf number mltipleNumber = number % mltipleNumber = 0
+
+    seq { 1 .. 999 }
+    |> Seq.filter(fun x -> isMultipleOf x 3 || isMultipleOf x 5)
+    |> Seq.sum
