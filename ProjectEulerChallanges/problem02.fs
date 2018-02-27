333ea57faaff2b825722bfdf8b3093a964b574b0
Ashley Barrett <ashleybarrett.mail@googlemail.com>
2018-02-27 16:47:02 +0000

    revision

--- ProjectEulerChallanges/problem02.fs
+++ ProjectEulerChallanges/problem02.fs
@@ -3,9 +3,11 @@ module Problem02
 let solution =
     //https://projecteuler.net/problem=2
 
+    let maxNumber = 4000000
+
     (1, 1)
     |> Seq.unfold(fun (num, acc) -> Some((num,acc),(acc, num + acc)))
-    |> Seq.takeWhile(fun (_, acc) -> acc < 4000000)
+    |> Seq.takeWhile(fun (_, acc) -> acc < maxNumber)
     |> Seq.map(fun (_, acc) -> acc)
     |> Seq.filter(fun x -> x % 2 = 0)
     |> Seq.sum
