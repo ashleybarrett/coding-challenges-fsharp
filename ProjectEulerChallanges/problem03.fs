36b9aa7502f3aed3c539d70be8d9ae2d6d89e39f
Ashley Barrett <ashleybarrett.mail@googlemail.com>
2018-02-28 09:56:11 +0000

    solution 3

---
+++ ProjectEulerChallanges/problem03.fs
@@ -0,0 +1,21 @@
+module Problem03
+open System.Web.Security.AntiXss
+
+let solution =
+    //https://projecteuler.net/problem=2
+
+    //let testNumber = 13195L
+    let testNumber = 600851475143L
+
+    let isFactor number candiate = number % candiate = 0L
+
+    let isPrime number = 
+        let oneLess = number - 1L
+        seq { 2L .. oneLess }
+        |> Seq.forall(fun x -> number % x <> 0L)
+
+    seq { 3L .. 2L .. testNumber }
+    |> Seq.filter(fun x -> isFactor testNumber x)
+    |> Seq.filter(isPrime)
+    |> Seq.iter(fun x -> printfn "%A" x)
+
