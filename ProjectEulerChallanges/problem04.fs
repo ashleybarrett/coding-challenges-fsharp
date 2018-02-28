4113ba40af3391b2e73f62468817707941560123
Ashley Barrett <ashleybarrett.mail@googlemail.com>
2018-02-28 14:59:38 +0000

    solition 4

--- ProjectEulerChallanges/problem04.fs
+++ ProjectEulerChallanges/problem04.fs
@@ -1,8 +1,28 @@
 module Problem03
-open System.Web.Security.AntiXss
 
 let solution =
     //https://projecteuler.net/problem=4
 
+    let uppernumber = 999
+    let lowerNumber = 100
+
+    let isPalindrome number = 
+        let numberAsString = number |> string
+        
+        let numberAsStringRev = 
+            numberAsString 
+            |> Seq.map(string)
+            |> Seq.rev 
+            |> String.concat ""
+        
+        numberAsString = numberAsStringRev
+
+    seq { 
+        for first in lowerNumber .. uppernumber do
+        for second in lowerNumber .. uppernumber do 
+            yield first * second
+     }
+     |> Seq.filter(isPalindrome)
+     |> Seq.max
 
 
