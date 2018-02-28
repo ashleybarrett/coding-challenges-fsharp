30661170a316a5f1b53bc8f30e8cff9c9857a143
Ashley Barrett <ashleybarrett.mail@googlemail.com>
2018-02-28 15:54:30 +0000

    solution 5

---
+++ ProjectEulerChallanges/problem05.fs
@@ -0,0 +1,20 @@
+module Problem03
+
+let solution =
+    //https://projecteuler.net/problem=4
+
+    let upperNumber = 20
+    let lowerNumber = 1
+
+    let dividesByAll candiate lowerNumber upperNumber =
+        seq { lowerNumber .. upperNumber }
+        |> Seq.forall(fun x -> candiate % x = 0)
+        
+    Seq.unfold(fun x -> Some(x, x + 1)) upperNumber
+    |> Seq.map(fun x -> x * upperNumber)
+    |> Seq.filter(fun x -> dividesByAll x lowerNumber upperNumber)
+    |> Seq.head
+    |> printfn "%A"
+
+
+
