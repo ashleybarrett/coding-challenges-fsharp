711e2bb27d380badb1415d3e951b0529a5926a81
Ashley Barrett <ashleybarrett.mail@googlemail.com>
2018-12-12 22:09:10 +0000

    small refactor

--- src/Challenges.ConsoleRunner/AdventOfCode/2018/Day02/Challenge02.fs
+++ src/Challenges.ConsoleRunner/AdventOfCode/2018/Day02/Challenge02.fs
@@ -6,15 +6,15 @@ let solution =
 
   let filePath = "src/Challenges.ConsoleRunner/AdventOfCode/2018/Day02/Input.txt"
 
-  let fileLinesMapped = File.ReadAllLines(filePath)
-  let halfLength = (fileLinesMapped |> Seq.length) / 2
-  let boxIdLength = fileLinesMapped |> Seq.head |> String.length
+  let boxIds = File.ReadAllLines(filePath)
+  let boxIdsHalfLength = (boxIds |> Seq.length) / 2
+  let boxIdLength = boxIds |> Seq.head |> String.length
  
   let mapToStringArray (a:string) = a.ToCharArray() |> Array.map string
 
   seq {
-      for outer in fileLinesMapped |> Seq.take halfLength do
-      for inner in fileLinesMapped do
+      for outer in boxIds |> Seq.take boxIdsHalfLength do
+      for inner in boxIds do
         match outer <> inner with
         | true -> yield (mapToStringArray outer, mapToStringArray inner)
         | _ -> ()
